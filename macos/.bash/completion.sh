fdc20cb8e67819a7ee58719f8edcfee775d3f8bf
Francisco Sicardi <francisco.sicardi@mercadolibre.com>
2023-02-13 15:04:31 -0300

    source git prompt explicitply in macos

--- macos/.bash/completion.sh
+++ macos/.bash/completion.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [ -f $(brew --prefix)/etc/bash_completion ]; then
-  . $(brew --prefix)/etc/bash_completion
+if [ -f $(brew --prefix)/etc/profile.d/bash_completion.sh ]; then
+  . $(brew --prefix)/etc/profile.d/bash_completion.sh
   __git_complete g __git_main
   complete -o bashdefault -o default -o nospace -F _docker d
+  . /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-prompt.sh
 fi
-
 
